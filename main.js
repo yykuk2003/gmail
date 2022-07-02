528529b4924a6999bc9513655ce7bb2269cdeae3
Guest <guest-kzqzkw@h25727.sgb.skir.kr>
2022-07-02 19:56:10 +0900

    add toggle bar

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const menuToggleIcon = document.getElementById('menu-toggle-icon');
 const navMobile = document.getElementById('nav');
 const headerElement = document.getElementById('header');
-
+//const menuToggleIcon = document.getElementById();
 
 //Toggle navigation
 const toggleMenu = () => {
@@ -10,3 +10,14 @@ const toggleMenu = () => {
 }
 
 menuToggleIcon.addEventListener('click', toggleMenu);
+
+
+const headerScrollEvent = () => {
+    if (this.scrollY >= 30) {
+        headerElement.classList.add('active-scroll');
+    } else {
+        headerElement.classList.remove('active-scroll');
+    }
+}
+
+window.addEventListener('scroll', headerScrollEvent)
